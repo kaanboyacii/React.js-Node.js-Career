3d4df1d4e81e3c206e81208580d3d112baa852d7
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-10-26 10:54:38 +0300

    App Setup

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../../components/navbar/Navbar";
+import "./home.scss";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="homeContainer">
+        <Navbar />
+      </div>
+    </div>
+  );
+};
+
+export default Home;

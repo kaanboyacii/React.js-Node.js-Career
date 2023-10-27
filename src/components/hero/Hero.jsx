7465c1a61a8f06415bc74ac58140cd7094387a94
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-10-27 11:34:07 +0300

    Search input added on hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,7 +1,20 @@
 import "./hero.scss";
 import { motion } from "framer-motion";
 import HeroImage from "../../img/hero.png";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import Autocomplete from "@mui/material/Autocomplete";
 
+const top10 = [
+  { title: "The Shawshank Redemption", year: 1994 },
+  { title: "The Godfather", year: 1972 },
+  { title: "The Godfather: Part II", year: 1974 },
+  { title: "The Dark Knight", year: 2008 },
+  { title: "Inglourious Basterds", year: 2009 },
+  { title: "Snatch", year: 2000 },
+  { title: "3 Idiots", year: 2009 },
+  { title: "Monty Python and the Holy Grail", year: 1975 },
+];
 const textVariants = {
   initial: {
     x: -500,
@@ -48,7 +61,9 @@ const Hero = () => {
           initial="initial"
           animate="animate"
         >
-          <motion.h2 variants={textVariants}>Sana Özel Fırsatları Yakala</motion.h2>
+          <motion.h2 variants={textVariants}>
+            Sana Özel Fırsatları Yakala
+          </motion.h2>
           <motion.h1 variants={textVariants}>Kariyerin Olsun</motion.h1>
           <motion.img
             variants={textVariants}
@@ -57,6 +72,26 @@ const Hero = () => {
             alt=""
           />
         </motion.div>
+        <div className="searchContainer">
+          <Stack spacing={2} sx={{ width: 300 }}>
+            <Autocomplete
+              freeSolo
+              id="free-solo-1-demo"
+              disableClearable
+              options={top10.map((option) => option.title)}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Kariyerini Arama Başla..."
+                  InputProps={{
+                    ...params.InputProps,
+                    type: "search",
+                  }}
+                />
+              )}
+            />
+          </Stack>
+        </div>
       </div>
       <motion.div
         className="slidingTextContainer"

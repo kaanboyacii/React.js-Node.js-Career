0dafc16dab52f0cc1b8cad87daae68dd752f04c2
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-10-27 16:35:18 +0300

    Css problem fixed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./home.scss";
 import Navbar from "../../components/navbar/Navbar";
 import Hero from "../../components/hero/Hero";
-import Events from "../../components/events/Events";
 import About from "../../components/about/About";
 import Services from "../../components/services/Services"; // Services bileşenini içeri aktardığınızdan emin olun
 
@@ -17,9 +16,6 @@ const Home = () => {
         <Services />
       </div>
       <div className="about">
-      {/* <div className="events">
-        <Events />
-      </div> */}
         <About />
       </div>
     </div>

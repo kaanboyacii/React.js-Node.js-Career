efe4d88e145beb7a598666271380040b6328651d
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-11-03 15:50:14 +0300

    Panel sidebar

--- client/src/pages/panel/Panel.jsx
+++ client/src/pages/panel/Panel.jsx
@@ -2,12 +2,57 @@ import "./panel.scss";
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
 import Footer from "../../components/footer/Footer";
+import HomeIcon from "@mui/icons-material/Home";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import SettingsIcon from "@mui/icons-material/Settings";
+import DescriptionIcon from "@mui/icons-material/Description";
+import RocketLaunchIcon from "@mui/icons-material/RocketLaunch";
+import WorkIcon from "@mui/icons-material/Work";
+import EmailIcon from "@mui/icons-material/Email";
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const Panel = () => {
   return (
     <div className="panel">
       <Navbar />
-      <div className="panel-container"></div>
+      <div className="panel-container">
+        <div className="sidebar">
+          <h1>Yönetim</h1>
+          <div className="sidebar-item">
+            <HomeIcon />
+            <span>Panel</span>
+          </div>
+          <div className="sidebar-item">
+            <DescriptionIcon />
+            <span>CV Oluştur</span>
+          </div>
+          <div className="sidebar-item">
+            <WorkIcon />
+            <span>Başvurularım</span>
+          </div>
+          <div className="sidebar-item">
+            <RocketLaunchIcon />
+            <span>Kişisel Gelişim Planı</span>
+          </div>
+          <div className="sidebar-item">
+            <EmailIcon />
+            <span>Gelen Kutusu</span>
+          </div>
+          <div className="sidebar-item">
+            <AccountCircleIcon />
+            <span>Profil</span>
+          </div>
+          <div className="sidebar-item">
+            <SettingsIcon />
+            <span>Ayarlar</span>
+          </div>
+          <div className="sidebar-item">
+            <LogoutIcon />
+            <span>Çıkış Yap</span>
+          </div>
+        </div>
+        <div className="content">content</div>
+      </div>
       <Footer />
     </div>
   );

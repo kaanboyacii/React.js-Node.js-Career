e7411398c9adaaa7b3a2d2792a0da6c793c985ed
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-11-08 10:34:31 +0300

    Settings tabs

--- client/src/components/panel/settings/Settings.jsx
+++ client/src/components/panel/settings/Settings.jsx
@@ -1,10 +1,77 @@
-import React from "react";
+import * as React from "react";
 import "./settings.scss";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+
+function CustomTabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+CustomTabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
 
 const Settings = () => {
-  return <div className="settings-card">
-    <h1>Ayarlar</h1>
-  </div>;
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  return (
+    <div className="settings-card">
+      <h1>Ayarlar</h1>
+      <Box sx={{ width: "100%" }}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            aria-label="basic tabs example"
+          >
+            <Tab label="Üyelik Ayarları" {...a11yProps(0)} />
+            <Tab label="İletişim Ayarları" {...a11yProps(1)} />
+            <Tab label="Uygulama Ayarları" {...a11yProps(2)} />
+          </Tabs>
+        </Box>
+        <CustomTabPanel value={value} index={0}>
+          Item One
+        </CustomTabPanel>
+        <CustomTabPanel value={value} index={1}>
+          Item Two
+        </CustomTabPanel>
+        <CustomTabPanel value={value} index={2}>
+          Item Three
+        </CustomTabPanel>
+      </Box>
+    </div>
+  );
 };
 
 export default Settings;

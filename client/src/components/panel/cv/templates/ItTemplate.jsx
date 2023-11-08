29848833f1c0b2ed780037baac410e9db4143d55
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-11-08 16:03:42 +0300

    Cv generator templates

---
+++ client/src/components/panel/cv/templates/ItTemplate.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const ItTemplate = ({ cvData }) => {
+  const { ad, doğumTarihi, eğitim, deneyim, beceriler } = cvData;
+
+  const docDefinition = {
+    content: [
+      { text: ad, fontSize: 20, bold: true },
+      { text: `Doğum Tarihi: ${doğumTarihi}` },
+      { text: "Eğitim", style: "sectionHeader" },
+      {
+        ul: eğitim.map((eğitimItem) => ({
+          text: `${eğitimItem.okul}, ${eğitimItem.bölüm} (${eğitimItem.mezuniyetYılı})`,
+        })),
+      },
+      { text: "Deneyim", style: "sectionHeader" },
+      {
+        ul: deneyim.map((deneyimItem) => ({
+          text: `${deneyimItem.şirket}, ${deneyimItem.pozisyon} (${deneyimItem.başlangıçTarihi} - ${deneyimItem.bitişTarihi})`,
+        })),
+      },
+      { text: "Beceriler", style: "sectionHeader" },
+      {
+        ul: beceriler.map((beceri) => ({
+          text: beceri,
+        })),
+      },
+    ],
+    styles: {
+      sectionHeader: { fontSize: 16, bold: true, margin: [0, 10, 0, 5] },
+    },
+  };
+
+  return docDefinition;
+};
+
+export default ItTemplate;

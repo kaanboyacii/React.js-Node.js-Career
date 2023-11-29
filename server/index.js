5ce1eac761a15781e4af2c81f1d0404d1dc2466f
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-11-29 14:41:15 +0300

    mongodb connection

--- server/index.js
+++ server/index.js
@@ -7,5 +7,34 @@ const app = express();
 dotenv.config();
 
 
+const connectDB = () => {
+    mongoose.set("strictQuery", false);
+    mongoose
+        .connect(process.env.MONGO)
+        .then(() => {
+            console.log("Connected to DB")
+        })
+        .catch((err) => {
+            console.error("MongoDB connection error:", err);
+            throw err;
+        });
+}
+
 app.use(express.json());
 app.use(cookieParser());
+
+
+app.use((err, req, res, next)=>{
+    const status = err.status || 500;
+    const message = err.message || "Something went wrong !";
+    return res.status(status).json({
+        success:false,
+        status:status,
+        message:message,
+    });
+});
+
+app.listen(8800, () => {
+    connectDB();
+    console.log("Connected to Server!")
+})

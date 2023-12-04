ab32d337a6524e0c333f9fd3478d8fde911c67d3
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-12-04 11:38:39 +0300

    verify token updated

--- server/utilities/verifyToken.js
+++ server/utilities/verifyToken.js
@@ -3,17 +3,17 @@ import jwt from "jsonwebtoken";
 const config = process.env;
 
 export const verifyToken = (req, res, next) => {
-  const token =
-    req.body.token || req.query.token || req.headers["x-access-token"];
+  const token = req.cookies.access_token;
 
   if (!token) {
     return res.status(403).send("A token is required for authentication");
   }
+
   try {
-    const decoded = jwt.verify(token, config.TOKEN_KEY);
+    const decoded = jwt.verify(token, config.JWT_SECRET);
     req.user = decoded;
+    next();
   } catch (err) {
     return res.status(401).send("Invalid Token");
   }
-  return next();
 };

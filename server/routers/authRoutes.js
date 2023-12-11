dffba374dac0e47193935b950da92968c181fa8e
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-12-11 12:20:45 +0300

    company auth routes added

--- server/routers/authRoutes.js
+++ server/routers/authRoutes.js
@@ -1,18 +1,24 @@
 import express from "express";
 import { googleAuth, logout, signin, signup } from "../controllers/authController.js";
+import { companySignin, companySignup, companyLogout } from "../controllers/companyAuthController.js";
 
 const router = express.Router();
-
+//USER AUTH
 //Sign up
-router.post("/signup",signup)
-
+router.post("/signup", signup)
 //Sign in
-router.post("/login",signin)
-
-//Sign in
-router.post("/logout",logout)
-
+router.post("/login", signin)
+//Logout
+router.post("/logout", logout)
 //Google auth
 router.post("/google", googleAuth)
+
+//COMPANY AUTH
+//Sign up
+router.post("/company-signup", companySignup)
+//Sign in
+router.post("/company-login", companySignin)
+//Logout
+router.post("/company-logout", companyLogout)
 
 export default router;

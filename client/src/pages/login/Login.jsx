9a9f67c068072acda991546a744b30ba8ecc0432
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-12-16 16:42:14 +0300

    Login page design changed

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,8 +1,8 @@
 import "./login.scss";
 import React from "react";
-import PeopleImage from "../../img/people.webp";
-import Logo from "../../img/logo.png";
-import { motion } from "framer-motion"; 
+import Logo from "../../img/logo-back.png";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   const containerVariants = {
@@ -14,50 +14,48 @@ const Login = () => {
     <motion.div
       className="login"
       initial="hidden"
-      animate="visible" 
-      variants={containerVariants} 
+      animate="visible"
+      variants={containerVariants}
     >
-      <div className="left-side">
-        <img src={PeopleImage} alt="Resim" />
-      </div>
-      <div className="right-side">
-        <div className="form-container">
-          <form>
-            <a href="/">
-              <img src={Logo} alt="Logo" />
-            </a>
-            <h1>
-              ineedcareer'e <br /> Giriş Yap
-            </h1>
-            <hr />
-            <div className="form-group">
-              <input type="email" name="email" placeholder="E-posta" required />
+      <div className="form-container">
+        <form>
+          <a href="/">
+            <img src={Logo} alt="Logo" />
+          </a>
+          <h1>
+            ineedcareer'e <br /> Giriş Yap
+          </h1>
+          <hr />
+          <div className="form-group">
+            <input type="email" name="email" placeholder="E-posta" required />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              name="password"
+              placeholder="Şifre"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <div className="remember-me">
+              <label htmlFor="rememberMe">Beni Hatırla</label>
+              <input type="checkbox" id="rememberMe" name="rememberMe" />
             </div>
-            <div className="form-group">
-              <input
-                type="password"
-                name="password"
-                placeholder="Şifre"
-                required
-              />
-            </div>
-            <div className="form-group">
-              <div className="remember-me">
-                <label htmlFor="rememberMe">Beni Hatırla</label>
-                <input type="checkbox" id="rememberMe" name="rememberMe" />
-              </div>
-              <a href="/sifremi-unuttum">Şifremi Unuttum</a>
-            </div>
-            <button className="login-button">Giriş Yap</button>
-            <div className="signup-link">
-              <span>Hesabınız yok mu ?</span>
-              <a href="/signup">Üye Ol</a>
-            </div>
-          </form>
-        </div>
+            <a href="/sifremi-unuttum">Şifremi Unuttum</a>
+          </div>
+          <button className="login-button">Giriş Yap</button>
+          <div className="signup-link">
+            <span>Hesabınız yok mu ?</span>
+            <a href="/signup">Üye Ol</a>
+            <br />
+            <Link to="/company-login">
+              <button className="company-login-button">Şirket Giriş</button>
+            </Link>
+          </div>
+        </form>
       </div>
     </motion.div>
   );
 };
-
 export default Login;

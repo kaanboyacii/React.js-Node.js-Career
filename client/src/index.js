50f0118027d42744ded46751a4bdbfb875d09ce6
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-12-20 10:28:16 +0300

    Redux in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { Provider } from 'react-redux';
+import { persistor, store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 import { DarkModeContextProvider } from "./context/darkModeContext";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <DarkModeContextProvider>
-      <App />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
     </DarkModeContextProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );

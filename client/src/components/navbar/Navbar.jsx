9947d9798745d7a658eca07db05b78ad4ccdcf9d
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-12-21 10:19:01 +0300

    Navbar user auth

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,16 +1,41 @@
 import "./navbar.scss";
-import { DarkModeContext } from "../../context/darkModeContext";
 import { useContext, useState, useEffect } from "react";
-import { motion } from "framer-motion";
 import Logo from "../../img/logo.png";
-import MenuIcon from "@mui/icons-material/Menu";
 import { Link } from "react-router-dom";
+import MenuIcon from "@mui/icons-material/Menu";
+import {
+  Menu,
+  MenuItem,
+  Avatar,
+  Typography,
+} from "@mui/material";
+import { useSelector, useDispatch } from "react-redux";
+import { logout } from "../../redux/userSlice.js";
+import axios from "axios";
 
 const Navbar = () => {
-  const { dispatch } = useContext(DarkModeContext);
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector((state) => state.user);
 
-  // Ekran genişliği değiştiğinde durumu izlemek için useEffect kullanıyoruz.
+  const handleMenuClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const openMenu = Boolean(anchorEl);
+
+  const handleLogout = async (e) => {
+    window.location.href = "/";
+    e.preventDefault();
+    dispatch(logout());
+    const res = await axios.post("/auth/logout");
+  };
+
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth > 768) {
@@ -46,8 +71,45 @@ const Navbar = () => {
           <a href="/contact">İletişim</a>
         </div>
         <div className="user-actions">
-          <a href="/login">Giriş Yap</a>
-          <a href="/signup">Üye Ol</a>
+          {currentUser ? (
+            <>
+              <Avatar
+                alt={currentUser.name}
+                src={currentUser.img}
+                onClick={handleMenuClick}
+                disableScrollLock
+              />
+              <Menu
+                anchorEl={anchorEl}
+                open={openMenu}
+                onClose={handleMenuClose}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "right",
+                }}
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                disableScrollLock
+              >
+                <MenuItem component={Link} to="/panel">
+                  <Typography>Profilim</Typography>
+                </MenuItem>
+                <MenuItem component={Link} to="/panel">
+                  <Typography>Ayarlar</Typography>
+                </MenuItem>
+                <MenuItem onClick={handleLogout}>
+                  <Typography>Çıkış Yap</Typography>
+                </MenuItem>
+              </Menu>
+            </>
+          ) : (
+            <>
+              <Link to="/login">Giriş Yap</Link>
+              <Link to="/signup">Üye Ol</Link>
+            </>
+          )}
         </div>
         <MenuIcon className="menu-icon" onClick={toggleMobileMenu} />
         {isMobileMenuOpen && (

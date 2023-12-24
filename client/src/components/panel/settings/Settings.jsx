a52c142cb3788f2304be14442a7a9a86d4c36d49
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-12-24 10:35:24 +0300

    Setting sms, email checkbox settings

--- client/src/components/panel/settings/Settings.jsx
+++ client/src/components/panel/settings/Settings.jsx
@@ -1,5 +1,6 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import "./settings.scss";
+import axios from "axios";
 import PropTypes from "prop-types";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
@@ -9,6 +10,9 @@ import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Switch from "@mui/material/Switch";
 import TextField from "@mui/material/TextField";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { updateProfile } from "../../../redux/userSlice";
 
 function CustomTabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -48,22 +52,175 @@ const Settings = () => {
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
-  const [oldPassword, setOldPassword] = React.useState("");
-  const [newPassword, setNewPassword] = React.useState("");
-  const [confirmPassword, setConfirmPassword] = React.useState("");
+  const { currentUser } = useSelector((state) => state.user);
+  const [emailSet, setEmailSet] = useState(false);
+  const [smsSet, setSmsSet] = useState(false);
+  const [notificationSet, setNotificationSet] = useState(false);
+  const [colorSet, setColorSet] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [oldPassword, setOldPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [newEmail, setNewEmail] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const handlePasswordChange = () => {
-    if (oldPassword && newPassword && newPassword === confirmPassword) {
-      // Şifre değiştirme işlemi başarılı
-      alert("Şifre değiştirme başarılı!");
-      setOldPassword("");
-      setNewPassword("");
-      setConfirmPassword("");
-    } else {
-      // Şifre değiştirme işlemi başarısız
-      alert(
-        "Şifre değiştirme işlemi başarısız. Lütfen girdiğiniz bilgileri kontrol edin."
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await axios.get(`/users/find/${currentUser._id}`);
+        const userData = response.data;
+        setEmailSet(userData.emailSet);
+        setSmsSet(userData.smsSet);
+        setNotificationSet(userData.notificationSet);
+        setColorSet(userData.colorSet);
+      } catch (error) {
+        console.error("Failed to fetch user data");
+      }
+    };
+
+    fetchUserData();
+  }, [currentUser._id]);
+
+  const handleEmailToggle = async () => {
+    try {
+      const response = await axios.put(`/users/${currentUser._id}`, {
+        emailSet: !emailSet,
+      });
+
+      if (response.status === 200) {
+        setEmailSet(!emailSet);
+      } else {
+        console.error("Failed to update email settings");
+      }
+    } catch (error) {
+      console.error("Failed to update email settings", error);
+    }
+  };
+
+  const handleSmsToggle = async () => {
+    try {
+      const response = await axios.put(`/users/${currentUser._id}`, {
+        smsSet: !smsSet,
+      });
+
+      if (response.status === 200) {
+        setSmsSet(!smsSet);
+      } else {
+        console.error("Failed to update SMS settings");
+      }
+    } catch (error) {
+      console.error("Failed to update SMS settings", error);
+    }
+  };
+
+  const handleNotificationToggle = async () => {
+    try {
+      const response = await axios.put(`/users/${currentUser._id}`, {
+        notificationSet: !notificationSet,
+      });
+
+      if (response.status === 200) {
+        setNotificationSet(!notificationSet);
+      } else {
+        console.error("Failed to update notification settings");
+      }
+    } catch (error) {
+      console.error("Failed to update notification settings", error);
+    }
+  };
+
+  const handleColorToggle = async () => {
+    try {
+      const response = await axios.put(`/users/${currentUser._id}`, {
+        colorSet: !colorSet,
+      });
+
+      if (response.status === 200) {
+        setColorSet(!colorSet);
+      } else {
+        console.error("Failed to update color settings");
+      }
+    } catch (error) {
+      console.error("Failed to update color settings", error);
+    }
+  };
+
+  const handleChangeEmail = async () => {
+    try {
+      if (!newEmail) {
+        alert("Lütfen yeni e-posta adresini girin");
+        return;
+      }
+      const response = await axios.put(`/users/${currentUser._id}`, {
+        email: newEmail,
+      });
+      if (response.status === 200) {
+        dispatch(
+          updateProfile({
+            ...currentUser,
+            email: newEmail,
+          })
+        );
+        console.log("E-posta başarıyla değiştirildi");
+      } else {
+        console.error("Failed to update email");
+      }
+    } catch (error) {
+      console.error("Failed to update email", error);
+    }
+  };
+
+  const handleChangePassword = async () => {
+    try {
+      // Check if the old password is correct
+      const checkPasswordResponse = await axios.post(
+        `/users/check-password/${currentUser._id}`,
+        {
+          oldPassword: oldPassword,
+        }
       );
+
+      if (!oldPassword || !newPassword || !confirmPassword) {
+        alert("Lütfen tüm alanları doldurun");
+        return;
+      }
+
+      if (newPassword !== confirmPassword) {
+        alert("Şifreler uyuşmuyor");
+        return;
+      }
+
+      if (checkPasswordResponse.data.success) {
+        // If old password is correct, proceed to update the password
+        const updatePasswordResponse = await axios.put(
+          `/users/${currentUser._id}`,
+          {
+            password: newPassword,
+          }
+        );
+
+        if (updatePasswordResponse.status === 200) {
+          dispatch(
+            updateProfile({
+              ...currentUser,
+              password: newPassword,
+            })
+          );
+          console.log(
+            "Şifre başarıyla değiştirildi",
+            updatePasswordResponse.data
+          );
+          navigate("/panel");
+          window.location.reload();
+        } else {
+          console.error("Failed to update user profile");
+        }
+      } else {
+        alert("Eski şifre yanlış");
+      }
+    } catch (error) {
+      setErrorMessage("Eski şifre yanlış");
     }
   };
 
@@ -93,6 +250,7 @@ const Settings = () => {
               value={oldPassword}
               onChange={(e) => setOldPassword(e.target.value)}
             />
+            {errorMessage && <p className="error-message">{errorMessage}</p>}
             <TextField
               type="password"
               label="Yeni Şifre"
@@ -109,7 +267,7 @@ const Settings = () => {
               value={confirmPassword}
               onChange={(e) => setConfirmPassword(e.target.value)}
             />
-            <button className="save-button" onClick={handlePasswordChange}>
+            <button className="save-button" onClick={handleChangePassword}>
               Şifre Değiştir
             </button>
           </div>
@@ -117,11 +275,15 @@ const Settings = () => {
             <h2>E-posta Değiştir</h2>
             <TextField
               type="email"
-              label="Mevcut E-posta"
+              label="Yeni E-posta"
               variant="outlined"
               fullWidth
+              value={newEmail}
+              onChange={(e) => setNewEmail(e.target.value)}
             />
-            <button className="save-button">Gönder</button>
+            <button className="save-button" onClick={handleChangeEmail}>
+              Gönder
+            </button>
           </div>
           <div className="div">
             <h2>Üyeliğimi Sil</h2>
@@ -132,31 +294,35 @@ const Settings = () => {
           <FormGroup>
             <h3>E-posta Ayarları</h3>
             <FormControlLabel
-              control={<Switch defaultChecked />}
-              label="Yeni iş fırsatları hakkında e-posta almak istiyorum. "
+              control={
+                <Switch checked={emailSet} onChange={handleEmailToggle} />
+              }
+              label="Yeni iş fırsatları hakkında e-posta almak istiyorum."
             />
             <hr />
             <h3>SMS Ayarları</h3>
             <FormControlLabel
-              control={<Switch defaultChecked />}
-              label="
-              Yeni iş fırsatları hakkında sms almak istiyorum.  "
+              control={<Switch checked={smsSet} onChange={handleSmsToggle} />}
+              label="Yeni iş fırsatları hakkında SMS almak istiyorum."
             />
             <hr />
             <h3>Bildirim Ayarları</h3>
             <FormControlLabel
-              control={<Switch defaultChecked />}
-              label="
-              Yeni iş fırsatları hakkında bildirim almak istiyorum  "
+              control={
+                <Switch
+                  checked={notificationSet}
+                  onChange={handleNotificationToggle}
+                />
+              }
+              label="Yeni iş fırsatları hakkında bildirim almak istiyorum."
             />
           </FormGroup>
         </CustomTabPanel>
         <CustomTabPanel value={value} index={2}>
           <h3>Renk Ayarları</h3>
           <FormControlLabel
-            control={<Switch defaultChecked />}
-            label="
-            Karanlık Mod  "
+            control={<Switch checked={colorSet} onChange={handleColorToggle} />}
+            label="Karanlık Mod"
           />
         </CustomTabPanel>
       </Box>

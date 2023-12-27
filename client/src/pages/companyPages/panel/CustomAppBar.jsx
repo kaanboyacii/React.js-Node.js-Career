7cbe44f297904539d36796d35af15b14b16a9d73
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-12-27 15:55:15 +0300

    to separate appbar with sidebar

--- client/src/pages/companyPages/panel/CustomAppBar.jsx
+++ client/src/pages/companyPages/panel/CustomAppBar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
@@ -8,56 +8,219 @@ import MenuIcon from "@mui/icons-material/Menu";
 import MailIcon from "@mui/icons-material/MoveToInbox";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import AccountCircle from "@mui/icons-material/AccountCircle";
-import "./adminPanel.scss"
-import { useSelector } from "react-redux";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import AppsIcon from "@mui/icons-material/Apps";
+import LogoutIcon from "@mui/icons-material/Logout";
+import "./adminPanel.scss";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { companyLogout } from "../../../redux/companySlice";
 
-const CustomAppBar = ({ handleDrawerToggle, handleProfileMenuOpen, mobileMenuId, menuId, renderMobileMenu, renderMenu }) => {
-  const { currentCompany } = useSelector((state) => state.company);
 
-  return (
-    <AppBar position="static">
-      <Toolbar>
+const CustomAppBar = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState(null);
+  const dispatch = useDispatch();
+
+  const handleLogout = async (e) => {
+    window.location.href = "/company-login";
+    e.preventDefault();
+    dispatch(companyLogout());
+    const res = await axios.post("/auth/company-logout");
+  };
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+
+  const menuId = "primary-search-account-menu";
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profilim</MenuItem>
+      <MenuItem onClick={handleLogout}>Çıkış yap</MenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = "primary-search-account-menu-mobile";
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+          <Badge badgeContent={4} color="error">
+            <MailIcon />
+          </Badge>
+        </IconButton>
+        <p>Mesajlar</p>
+      </MenuItem>
+      <MenuItem>
         <IconButton
           size="large"
-          edge="start"
+          aria-label="show 17 new notifications"
           color="inherit"
-          aria-label="open drawer"
-          sx={{ mr: 2 }}
-          onClick={handleDrawerToggle}
         >
-          <MenuIcon />
+          <Badge badgeContent={17} color="error">
+            <NotificationsIcon />
+          </Badge>
         </IconButton>
-        <Typography variant="h6" noWrap component="div">
-          {currentCompany.name}
-        </Typography>
-        <div style={{ flexGrow: 1 }} />
-        <div style={{ display: { xs: "none", md: "flex" } }}>
-          <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-            <Badge badgeContent={4} color="error">
-              <MailIcon />
-            </Badge>
-          </IconButton>
-          <IconButton size="large" aria-label="show 17 new notifications" color="inherit">
-            <Badge badgeContent={17} color="error">
-              <NotificationsIcon />
-            </Badge>
-          </IconButton>
+        <p>Bildirimler</p>
+      </MenuItem>
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Profil</p>
+      </MenuItem>
+    </Menu>
+  );
+  const handleDrawerToggleLocal = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
+
+  const { currentCompany } = useSelector((state) => state.company);
+  const sidebarContent = (
+    <List>
+      <ListItem button>
+        <ListItemIcon>
+          <DashboardIcon />
+        </ListItemIcon>
+        <ListItemText primary="Yönetim Paneli" />
+      </ListItem>
+      <ListItem button>
+        <ListItemIcon>
+          <AppsIcon />
+        </ListItemIcon>
+        <ListItemText primary="İş İlanlarım" />
+      </ListItem>
+      <ListItem button>
+        <ListItemIcon>
+          <NotificationsIcon />
+        </ListItemIcon>
+        <ListItemText primary="Başvurular" />
+      </ListItem>
+      <ListItem button>
+        <ListItemIcon>
+          <AccountCircleIcon />
+        </ListItemIcon>
+        <ListItemText primary="Profil" />
+      </ListItem>
+      <ListItem button onClick={handleProfileMenuOpen}>
+        <ListItemIcon>
+          <LogoutIcon />
+        </ListItemIcon>
+        <ListItemText primary="Çıkış Yap" />
+      </ListItem>
+    </List>
+  );
+
+  return (
+    <React.Fragment>
+      <AppBar position="static">
+        <Toolbar>
           <IconButton
             size="large"
-            edge="end"
-            aria-label="account of current user"
-            aria-controls={menuId}
-            aria-haspopup="true"
-            onClick={handleProfileMenuOpen}
+            edge="start"
             color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+            onClick={handleDrawerToggleLocal}
           >
-            <AccountCircle />
+            <MenuIcon />
           </IconButton>
-        </div>
-      </Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            {currentCompany.name}
+          </Typography>
+          <div style={{ flexGrow: 1 }} />
+          <div style={{ display: { xs: "none", md: "flex" } }}>
+            <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+              <Badge badgeContent={4} color="error">
+                <MailIcon />
+              </Badge>
+            </IconButton>
+            <IconButton size="large" aria-label="show 17 new notifications" color="inherit">
+              <Badge badgeContent={17} color="error">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              size="large"
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+      <Drawer anchor="left" open={isDrawerOpen} onClose={handleDrawerToggleLocal}>
+        {sidebarContent}
+      </Drawer>
       {renderMobileMenu}
       {renderMenu}
-    </AppBar>
+    </React.Fragment>
   );
 };
 

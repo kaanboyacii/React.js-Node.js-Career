edc77030846d6915fdcc7097fd5d13d00c9a8c5b
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-12-30 12:01:58 +0300

    Apply Job confirmation component

---
+++ client/src/components/messages/JobConfirmation.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Button from "@mui/material/Button";
+import "./jobConfirmation.scss";
+
+const JobConfirmation = ({ open, handleClose }) => {
+  const dialogStyle = {
+    overflow: "hidden",
+    borderRadius: "10px",
+  };
+
+  return (
+    <Dialog
+      disableScrollLock={true}
+      open={open}
+      onClose={handleClose}
+      PaperProps={{ style: dialogStyle }}
+    >
+      <DialogTitle className="dialog-title">İş Başvurusu Onayı</DialogTitle>
+      <DialogContent className="dialog-content">
+        <DialogContentText>
+          İş başvurunuz başarıyla alındı. Teşekkür ederiz!
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions className="dialog-actions">
+        <Button onClick={handleClose} color="primary">
+          Tamam
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default JobConfirmation;

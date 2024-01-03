44094a86a0b18368d4edcf9c2bdb34b09d3f862d
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2024-01-03 11:39:25 +0300

    event list page fetch data and listing

--- client/src/pages/events/Events.jsx
+++ client/src/pages/events/Events.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./events.scss";
 import Image from "../../img/software.jpg";
 import Navbar from "../../components/navbar/Navbar";
@@ -6,178 +6,83 @@ import Footer from "../../components/footer/Footer";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
-const eventsCardsData = [
-  {
-    title: "Software buluşması",
-    company: "İzmir Yazılım",
-    location: "İzmir / Konak",
-    category: "Yazılım",
-    date: "11.01.2024 11.00-17.00",
-    last_date: "11.01.2024",
-    img: Image,
-  },
-  {
-    title: "Web Geliştirme Semineri",
-    company: "WebMasters Akademi",
-    location: "Ankara / Çankaya",
-    category: "Yazılım",
-    date: "20.02.2024 11.00-17.00",
-    last_date: "11.01.2024",
-    img: Image,
-  },
-  {
-    title: "Mobil Uygulama Geliştirme Atölyesi",
-    company: "MobilTech Danışmanlık",
-    location: "İstanbul / Beşiktaş",
-    category: "Yazılım",
-    date: "15.03.2024 11.00-17.00",
-    last_date: "11.01.2024",
-    img: Image,
-  },
-  {
-    title: "Veri Analitiği Konferansı",
-    company: "DataMinds Derneği",
-    location: "Antalya / Lara",
-    category: "Yazılım",
-    date: "10.04.2024 11.00-17.00",
-    last_date: "11.01.2024",
-    img: Image,
-  },
-  {
-    title: "Veri Analitiği Konferansı",
-    company: "DataMinds Derneği",
-    location: "Antalya / Lara",
-    category: "Yazılım",
-    date: "10.04.2024 11.00-17.00",
-    last_date: "11.01.2024",
-    img: Image,
-  },
-  {
-    title: "Veri Analitiği Konferansı",
-    company: "DataMinds Derneği",
-    location: "Antalya / Lara",
-    category: "Yazılım",
-    date: "10.04.2024 11.00-17.00",
-    last_date: "11.01.2024",
-    img: Image,
-  },
-  {
-    title: "Veri Analitiği Konferansı",
-    company: "DataMinds Derneği",
-    location: "Antalya / Lara",
-    category: "Yazılım",
-    date: "10.04.2024",
-    last_date: "11.01.2024",
-    img: Image,
-  },
-  {
-    title: "Veri Analitiği Konferansı",
-    company: "DataMinds Derneği",
-    location: "Antalya / Lara",
-    category: "Yazılım",
-    date: "10.04.2024 11.00-17.00",
-    last_date: "11.01.2024",
-    img: Image,
-  },
-  {
-    title: "Veri Analitiği Konferansı",
-    company: "DataMinds Derneği",
-    location: "Antalya / Lara",
-    category: "Yazılım",
-    date: "10.04.2024 11.00-17.00",
-    last_date: "11.01.2024",
-    img: Image,
-  },
-  {
-    title: "Veri Analitiği Konferansı",
-    company: "DataMinds Derneği",
-    location: "Antalya / Lara",
-    category: "Yazılım",
-    date: "10.04.2024",
-    last_date: "11.01.2024",
-    img: Image,
-  },
-  {
-    title: "Veri Analitiği Konferansı",
-    company: "DataMinds Derneği",
-    location: "Antalya / Lara",
-    category: "Yazılım",
-    date: "10.04.2024 11.00-17.00",
-    last_date: "11.01.2024",
-    img: Image,
-  },
-  {
-    title: "Veri Analitiği Konferansı",
-    company: "DataMinds Derneği",
-    location: "Antalya / Lara",
-    category: "Yazılım",
-    date: "10.04.2024 11.00-17.00",
-    last_date: "11.01.2024",
-    img: Image,
-  },
-  {
-    title: "Veri Analitiği Konferansı",
-    company: "DataMinds Derneği",
-    location: "Antalya / Lara",
-    category: "Yazılım",
-    date: "10.04.2024",
-    last_date: "11.01.2024",
-    img: Image,
-  },
-];
-
 const Events = () => {
   const [visibleEventsCards, setVisibleEventsCards] = useState(6);
+  const [eventCardsData, setEventCardsData] = useState([]);
+  const showMoreButton = eventCardsData.length > 8;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/events/getAllEvents");
+        const data = await response.json();
+        setEventCardsData(data);
+      } catch (error) {
+        console.error("Error fetching event data:", error);
+      }
+    };
 
+    fetchData();
+  }, []);
   return (
     <div className="events">
       <Navbar />
       <div className="events-container">
         <div className="left-side">
           <div className="events-cards">
-            {eventsCardsData
-              .slice(0, visibleEventsCards)
-              .map((event, index) => (
-                <motion.div
-                  className="event-card"
-                  initial={{ opacity: 0, scale: 0.8 }}
-                  animate={{ opacity: 1, scale: 1 }}
-                  transition={{ duration: 0.3 }}
-                >
-                  {" "}
-                  <img
-                    className="event-image"
-                    src={event.img}
-                    alt={event.title}
-                  />
-                  <div className="event-details">
-                    <Link to="/event">
-                      <h3>{event.title}</h3>
-                    </Link>
-                    <p>
-                      <strong>Şirket:</strong> {event.company}
-                    </p>
-                    <p>
-                      <strong>Lokasyon:</strong> {event.location}
-                    </p>
-                    <p>
-                      <strong>Tarih:</strong> {event.date}⏰
-                    </p>
-                    <p>
-                      <strong>Kategori:</strong> {event.category}
-                    </p>
-                    <span>
-                      <hr />
-                      Son Başvuru Tarihi: <strong>
-                        {event.last_date}
-                      </strong>
-                    </span>
-                  </div>
+            {eventCardsData.slice(0, visibleEventsCards).map((event, index) => (
+              <motion.div
+                className="event-card"
+                initial={{ opacity: 0, scale: 0.8 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 0.3 }}
+              >
+                {" "}
+                {/* <img
+                  className="event-image"
+                  src={event.img}
+                  alt={event.title}
+                /> */}
+                <div className="event-details">
                   <Link to="/event">
-                    <button>Etkinlik Detaylarını Gör</button>
+                    <h3>{event.title}</h3>
                   </Link>
-                </motion.div>
-              ))}
+                  <p>
+                    <strong>Şirket:</strong> {event.company.companyName}
+                  </p>
+                  <p>
+                    <strong>Lokasyon:</strong> {event.location}
+                  </p>
+                  <p>
+                    <strong>Tarih:</strong>
+                    {event.date
+                      ? new Date(event.date).toLocaleString("tr-TR", {
+                          dateStyle: "full",
+                          timeStyle: "short",
+                        })
+                      : ""}
+                    ⏰
+                  </p>
+                  <p>
+                    <strong>Kategori:</strong> {event.type}
+                  </p>
+                  <span>
+                    <hr />
+                    Son Başvuru Tarihi:{" "}
+                    <strong>
+                      {" "}
+                      {event.applicationDeadline
+                        ? new Date(
+                            event.applicationDeadline
+                          ).toLocaleDateString()
+                        : ""}
+                    </strong>
+                  </span>
+                </div>
+                <Link to={`/events/${event._id}`}>
+                  <button>Etkinlik Detaylarını Gör</button>
+                </Link>
+              </motion.div>
+            ))}
           </div>
         </div>
       </div>
@@ -187,9 +92,11 @@ const Events = () => {
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 0.3 }}
       >
-        <button onClick={() => setVisibleEventsCards(visibleEventsCards + 4)}>
-          Daha Fazla Göster
-        </button>
+        {showMoreButton && (
+          <button onClick={() => setEventCardsData(visibleEventsCards + 4)}>
+            Daha Fazla Göster
+          </button>
+        )}
       </motion.div>
       <Footer />
     </div>

7dec2e2c60b0bf9da2400f58307d36fe1d933af1
kaanboyacii <kaanboyacibn@gmail.com>
2024-05-05 17:17:06 +0300

    widgets setup on company panel

--- client/src/pages/companyPanelPages/CompanyPanel/CompanyPanel.jsx
+++ client/src/pages/companyPanelPages/CompanyPanel/CompanyPanel.jsx
@@ -3,100 +3,68 @@ import Layout from "../Layout";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
-import {
-  PieChart,
-  Pie,
-  LineChart,
-  Line,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-} from "recharts";
 import "./companyPanel.scss";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const CompanyPanel = () => {
-  const pieChartData = [
-    { name: "Group A", value: 400 },
-    { name: "Group B", value: 300 },
-    { name: "Group C", value: 300 },
-    { name: "Group D", value: 200 },
-  ];
-
-  const lineChartData = [
-    { name: "Jan", uv: 4000, pv: 2400, amt: 2400 },
-    { name: "Feb", uv: 3000, pv: 1398, amt: 2210 },
-    { name: "Mar", uv: 2000, pv: 9800, amt: 2290 },
-    { name: "Apr", uv: 2780, pv: 3908, amt: 2000 },
-    { name: "May", uv: 1890, pv: 4800, amt: 2181 },
-    { name: "Jun", uv: 2390, pv: 3800, amt: 2500 },
-    { name: "Jul", uv: 3490, pv: 4300, amt: 2100 },
-  ];
+  const { currentCompany } = useSelector((state) => state.company);
 
   return (
     <Layout>
       <div className="company-panel">
         <Grid container spacing={3}>
           <Grid item xs={12} sm={6} md={3}>
-            <Paper className="widget widget-1" elevation={3}>
-              <Typography variant="h6">Widget 1</Typography>
-              <Typography variant="body1">
-                Bu bir admin paneli widgetidir.
-              </Typography>
-            </Paper>
+            <Link
+              to="/company-panel/jobs"
+              className="widget-link"
+              style={{ textDecoration: "none" }}
+            >
+              <Paper className="widget widget-1" elevation={3}>
+                <Typography variant="h5">Toplam İş İlanı </Typography>
+                <Typography variant="h6">
+                  {currentCompany?.jobs?.length || "Loading..."}
+                </Typography>
+              </Paper>
+            </Link>
           </Grid>
           <Grid item xs={12} sm={6} md={3}>
-            <Paper className="widget widget-2" elevation={3}>
-              <Typography variant="h6">Widget 2</Typography>
-              <Typography variant="body1">
-                Bu bir admin paneli widgetidir.
-              </Typography>
-            </Paper>
+            <Link
+              to="/company-panel/events"
+              className="widget-link"
+              style={{ textDecoration: "none" }}
+            >
+              <Paper className="widget widget-2" elevation={3}>
+                <Typography variant="h5">Toplam Etkinlik </Typography>
+                <Typography variant="h6">
+                  {currentCompany?.events?.length || "Loading..."}
+                </Typography>
+              </Paper>
+            </Link>
           </Grid>
           <Grid item xs={12} sm={6} md={3}>
-            <Paper className="widget widget-3" elevation={3}>
-              <Typography variant="h6">Widget 3</Typography>
-              <Typography variant="body1">
-                Bu bir admin paneli widgetidir.
-              </Typography>
-            </Paper>
+            <Link
+              to="/company-panel"
+              className="widget-link"
+              style={{ textDecoration: "none" }}
+            >
+              <Paper className="widget widget-3" elevation={3}>
+                <Typography variant="h5">Profil Ziyareti</Typography>
+                <Typography variant="h6">262 </Typography>
+              </Paper>
+            </Link>
           </Grid>
           <Grid item xs={12} sm={6} md={3}>
-            <Paper className="widget widget-4" elevation={3}>
-              <Typography variant="h6">Widget 4</Typography>
-              <Typography variant="body1">
-                Bu bir admin paneli widgetidir.
-              </Typography>
-            </Paper>
-          </Grid>
-        </Grid>
-        <Grid container spacing={3}>
-          <Grid item xs={12} md={6}>
-            <Paper className="chart-container" elevation={12}>
-              <PieChart width={600} height={400}>
-                <Pie
-                  dataKey="value"
-                  isAnimationActive={false}
-                  data={pieChartData}
-                  fill="#0C356A"
-                  label
-                />
-              </PieChart>
-            </Paper>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <Paper className="chart-container" elevation={3}>
-              <LineChart width={600} height={400} data={lineChartData}>
-                <XAxis dataKey="name" />
-                <YAxis />
-                <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
-                <Tooltip />
-                <Legend />
-                <Line type="monotone" dataKey="pv" stroke="#0C356A" />
-                <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-              </LineChart>
-            </Paper>
+            <Link
+              to="/company-panel"
+              className="widget-link"
+              style={{ textDecoration: "none" }}
+            >
+              <Paper className="widget widget-4" elevation={3}>
+                <Typography variant="h5">Etkileşim Sayısı</Typography>
+                <Typography variant="h6">524</Typography>
+              </Paper>
+            </Link>
           </Grid>
         </Grid>
       </div>
